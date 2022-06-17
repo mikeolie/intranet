50e4c69a53a302cc008c8a5bc690f8fe46f19de1
Michael Olie <molie@rarecollectiblestv.com>
2022-06-17 14:32:37 -0700

    added origin and initialized project

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+globalThis.React = React;
+
+const container: HTMLElement = document.getElementById("root")!;
+const root = createRoot(container);
+
+const AppWrapper = () => (
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+root.render(AppWrapper());
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
