5db3683832ec7f429e948ba14a7803152d830646
Michael Olie <molie@rarecollectiblestv.com>
2022-06-29 15:54:52 -0700

    able to display current weather

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,14 +1,23 @@
 import { WEATHER_INITIAL_STATE } from "../../reducers/weather";
 
+import "./styles.scss";
+
 interface WeatherProps {
   data: WEATHER_INITIAL_STATE;
 }
 function Weather({ data }: WeatherProps) {
-  const { temp } = data
+  const { temp, description, icon } = data;
+  const formattedTemp = +temp.toFixed(0);
   return (
-    <article>
-      <h4></h4>
-    </article>
+    <section className="weather-info__container">
+      <img src={icon} alt={description} />
+      <article>
+        <h4>
+          {formattedTemp} <span>℉</span>
+        </h4>
+        {description}
+      </article>
+    </section>
   );
 }
 export default Weather;
