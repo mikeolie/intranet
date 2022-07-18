de361aea32ba188227aa097c4ec4cbeb135e36a4
Michael Olie <molie@rarecollectiblestv.com>
2022-07-18 11:05:54 -0700

    updated admin posts

--- src/components/AdminPosts/AdminPost.tsx
+++ src/components/AdminPosts/AdminPost.tsx
@@ -1,18 +1,23 @@
-import { REACT_POST } from "../../common/types";
+import ReactHTMLParser from "react-html-parser";
+
+import { POST } from "../../common/types";
 import DefaultImg from "../../assets/images/No_Image_Available.jpg";
 
-interface PostProps extends REACT_POST {}
+interface PostProps {
+  data: POST;
+}
 
 function Post({ data }: PostProps) {
   const { title, department, body, header_img: headerImg } = data;
   const imgToUse = headerImg !== "" ? headerImg : DefaultImg;
+  const bodyToShow = ReactHTMLParser(body.substring(0, 100));
   return (
     <li className="post__container">
       <img className="post__image" src={imgToUse} alt={title} />
       <article className="post__content">
         <sup>{title}</sup>
         <sub>{department}</sub>
-        <span>{body}</span>
+        {bodyToShow}
       </article>
     </li>
   );
